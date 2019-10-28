d026f59bc7abcba737a91b49a4cf2470afde0fff
Abdelaziz Mokhnache <m.abdelaziz1993@gmail.com>
2019-10-28 17:38:15 +0100

    test using baboon image instead of lena

--- test/index.js
+++ test/index.js
@@ -1,8 +1,6 @@
 const getPixels = require('get-pixels')
 const imshow = require('../src/index')
 
-getPixels('test/lena_gray_256.png', (err, pixels) => {
-  console.time('imshow lena image')
+getPixels('test/baboon_rgb_256.jpg', (err, pixels) => {
   imshow(pixels)
-  console.timeEnd('imshow lena image')
 })
